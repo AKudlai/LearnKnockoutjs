7d1c857e5da3cee2ea347317ce3b1c44f579a2d7
Alexander Kudlai <student@kudlai.com>
2019-12-19 12:22:34 +0200

    finished with text-binding, foreach-binding, visible-hidden-binding, class-css-binding.

--- LearnKnockoutjs/Scripts/app/employee.js
+++ LearnKnockoutjs/Scripts/app/employee.js
@@ -8,10 +8,16 @@ class Employee {
         this.lastName = ko.observable('Trust');
         this.age = ko.observable(18);
         this.email = ko.observable('alex@trust.com');
-        this.salary = ko.observable(100500);
+        this.salary = ko.pureComputed(() => this.selectedPosition() ? this.selectedPosition().salary : '');
         this.hired = ko.observable(false);
         this.fired = ko.observable(false);
-        this.position = ko.observableArray(['Developer', 'QA', 'BA', 'Sales Manager', 'Scrum Master', 'Not Defined']);
+        this.position = ko.observableArray([
+            { name: 'Developer', salary: 200 }, 
+            { name: 'QA', salary: 100 }, 
+            { name: 'BA', salary: 150 }, 
+            { name: 'Sales Manager', salary: 180 }, 
+            { name: 'Scrum Master', salary: 160 },
+            { name: 'Not Defined', salary: null }]);
         this.selectedPosition = ko.observable(false);
     }
 
@@ -35,7 +41,7 @@ class Employee {
 
     showSelectedPosition(data, event) {
         if (this.selectedPosition()) {
-            alert(this.selectedPosition());
+            alert(this.selectedPosition().name);
         }
     }
 }
